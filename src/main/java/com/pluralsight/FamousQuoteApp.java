6639bc67e2a005695c887214326a534eb33030ed
jefre <jefredujon@gmail.com>
2025-10-07 11:16:37 -0400

    first commit

--- src/main/java/com/pluralsight/FamousQuoteApp.java
+++ src/main/java/com/pluralsight/FamousQuoteApp.java
@@ -11,7 +11,13 @@ public class FamousQuoteApp {
                 "There is No Tommorrow Rocky There is No Tomorrow - Apollo Creed",
                 "Family. - Vin Diesel",
                 " Your time is Up My Time is Now - John Cena",
-                " The courageous fallen, the anguished fallen, their lives have meaning because we, the living refuse to forget them and as we ride to certain death, we trust our successors to do the same for us! Because my soldiers do not buckle or yield when faced with the cruelty of this world! MY SOLDIERS PUSH FORWARD! MY SOLDIERS SCREAM OUT! MY SOLDIERS RAGE! - Commander Erwin    "
+                " The courageous fallen, the anguished fallen, their lives have meaning because we, the living refuse to forget them and as we ride to certain death, we trust our successors to do the same for us! Because my soldiers do not buckle or yield when faced with the cruelty of this world! MY SOLDIERS PUSH FORWARD! MY SOLDIERS SCREAM OUT! MY SOLDIERS RAGE! - Commander Erwin",
+                " Hakuna Matata what a wonderful phrase Hakuna Matata i means no worry for the rest of your days - Lion King ",
+                " Is that supposed to be insulting? You say I'm arrogant, I say damn right. That's pride. Pride in the Saiyan I am: the mighty prince of the ultimate warrior race! Impossible to defeat you? Don't make me laugh! I won't surrender control, that's not the man I am! Let Kakarot use his thoughtless Ultra Instinct all he wants! But I'm the one who's going to crush you, Jiren! And I'll do it my way!" ,
+                "Be Like Paul - Paul ",
+                "You're either Perfect or not me - Perfect Cell",
+                "May the Force be with you. — Star Wars",
+                "My mama always said life was like a box of chocolates. — Forrest Gump "
 
         };
 
